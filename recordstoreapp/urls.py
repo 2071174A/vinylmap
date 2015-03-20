28ded2eb39a7a1e2b209266b951eec0437b449c6
2087829p <2087829p@student.gla.ac.uk>
2015-03-20 22:32:04 +0000

    backup231

--- recordstoreapp/urls.py
+++ recordstoreapp/urls.py
@@ -4,6 +4,7 @@ from recordstoreapp import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     #url(r'^backcatalogue/$', views.backCat, name='genrebackcatalogue'),
     #url(r'^future/$', views.forthcoming, name='genreforthcoming'),
--- recordstoreapp/views.py
+++ recordstoreapp/views.py
@@ -7,9 +7,15 @@ from django.db import connection
 # Home page
 def index(request):
     context_dict = {}
-    rec_list = Record.objects.all()[:25]
-    context_dict['rec_list'] = rec_list
+    rec_list = Record.objects.all()
+    total=len(rec_list)
+    pg=int(request.GET['page']) if 'page' in request.GET else 1
+    ub=min(pg*12,total-pg*12)
+    #[:12]
+    context_dict['rec_list'] = rec_list[(pg-1)*12:ub]
+    context_dict['range']=range(1,int(total/12))
     return render(request, 'index.html', context_dict)  # About page with description of tech used etc.
+
 def about(request):
     context_dict = {}
     return render(request, 'about.html', context_dict)
@@ -31,7 +37,7 @@ def search(request):
         rec=cursor.fetchall()
         cd['res'] = rec
         print rec
-    return render(request, 'search.html', cd)
+    return render(request, 'index.html', cd)
 
 
 def new_releases(request):
@@ -53,4 +59,4 @@ def record_view(request):
                 record = Record.objects.get(id=record_id)
                 context_dict['stores']=Store.objects.filter(record=record)#record.stores.all()
                 context_dict['rec'] = record
-    return render(request, 'record.html', context_dict)
+    return render(request, 'index.html', context_dict)

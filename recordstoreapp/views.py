f8ee11e568d70198decf6f22ac865b04984a016d
2074786m <2074786m@student.gla.ac.uk>
2015-03-20 23:54:12 +0000

    search bugfixes

--- recordstoreapp/views.py
+++ recordstoreapp/views.py
@@ -23,15 +23,22 @@ def contact(request):
 
 
 def search(request):
-    cd = {}
-    if 'q' in request.GET:
-        q = request.GET['q']
-        cursor = connection.cursor()
-        cursor.execute("SELECT id,title,artist FROM recordstoreapp_record WHERE title like '%" + q + "%' or artist like '%" + q + "%' or label like '%" + q + "%' or cat_no like '%" + q + "%';")
-        rec=cursor.fetchall()
-        cd['res'] = rec
-        print rec
-    return render(request, 'search.html', cd)
+	context_dict = {}
+	if 'q' in request.GET and request.GET['q'] != '':
+		q = request.GET['q']
+		cursor = connection.cursor()
+		cursor.execute("SELECT id,title,artist FROM recordstoreapp_record WHERE title like '%" + q + "%' or artist like '%" + q + "%' or label like '%" + q + "%' or cat_no like '%" + q + "%';")
+		rec_list=cursor.fetchall()
+		
+		total=len(rec_list)
+		pg=int(request.GET['page']) if 'page' in request.GET else 1
+		ub=min(pg*12,total-pg*12)
+
+		context_dict['rec_list'] = rec_list[(pg-1)*12:ub]
+		context_dict['range'] = range(1,int(total/12)+1)
+		context_dict['q'] = q
+
+	return render(request, 'search.html', context_dict)
 
 
 def new_releases(request):
@@ -44,7 +51,6 @@ def new_releases(request):
 
 	context_dict['rec_list'] = rec_list[(pg-1)*12:ub]
 	context_dict['range']=range(1,int(total/12))
-	#context_dict['release_list'] = fetch_releases(genre_url, type)
 	
 	return render(request, 'releases.html', context_dict)
 

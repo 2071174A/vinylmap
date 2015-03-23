dc5f867db4ec741673aaa5a46e76623ecfb1d795
Ross Anderson <rossanderson1993@gmail.com>
2015-03-23 22:43:19 +0000

    404 test case

--- recordstoreapp/tests.py
+++ recordstoreapp/tests.py
@@ -12,8 +12,17 @@ class RecordMethodTests(TestCase):
         record.save()
         self.assertEqual((record.title!=" "), True)
 
+
+
 class StoreMethodTests (TestCase):
     def test_ensure_store_has_link(self):
         store=Store(name= "RecordStore", link="www.recordstore.ac.uk")
         store.save()
         self.assertEqual((store.link!=" "), True)
+
+
+class IndexViewTests(TestCase):
+    #test that invalid record requests pull the 404 page
+    def test_index_view_with_no_categories(self):
+        response = self.client.get("/records/?record_id=-1")
+        self.assertContains(response, "Http Error 404")

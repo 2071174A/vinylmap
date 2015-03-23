a727c2b01180850485a6402f6118a3daeae1ac22
Kristina Lazarova <2088209l@student.gla.ac.uk>
2015-03-23 22:13:40 +0000

    some tests

--- recordstoreapp/tests.py
+++ recordstoreapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from recordstoreapp.models import Record, Store
 
-# Create your tests here.
+class RecordMethodTests(TestCase):
+    def test_ensure_title_is_not_empty_string(self):
+
+        """
+                ensure_title_is_not_empty_string should result True if the title is not an empty string
+        """
+        record = Record(title="Eclipse", artist="Twin Shadow")
+        record.save()
+        self.assertEqual((record.title!=" "), True)
+
+class StoreMethodTests (TestCase):
+    def test_ensure_store_has_link(self):
+        store=Store(name= "RecordStore", link="www.recordstore.ac.uk")
+        store.save()
+        self.assertEqual((store.link!=" "), True)
